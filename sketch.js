34f2b3c4b34842f272e31c13c789d12ece02d6d4
Unamta Aslam <aslamunamta@gmail.com>
2026-03-09 22:48:26 +0000

    feat: all instructions completed

--- sketch.js
+++ sketch.js
@@ -1,14 +1,71 @@
+let intervals_slider;
+let gap_slider;
+let intervals;
+let gap;
+let startHue_slider;
+let startHue;
+
 function setup() {
   createCanvas(1000, 1000);
   colorMode(HSB);
+  intervals_slider = createSlider(0, 20, 10, 1);
+  intervals_slider.position(600, 270);
+
+  gap_slider = createSlider(10, 30, 0, 1);
+  gap_slider.position(600, 465);
+
+  startHue_slider = createSlider(0, 360, 40, 1);
+  startHue_slider.position(600, 370);
 }
 
 function draw() {
+  gap = gap_slider.value();
+  intervals = intervals_slider.value();
+  startHue = startHue_slider.value();
+
   background(60);
+
+  textSize(30);
+  fill("black");
+  text("Monochromatic", 40, 150);
   monoPalette();
-  let startColor = color(40, 100, 100); // yellow-orange
-  let endColor = color(80, 100, 100); // yellow-green
-  drawPalette(startColor, endColor, 50, 520, 250, 12);
+  let interval = intervals;
+  let endHue = startHue + interval * 12;
+  let startColor = color(startHue, 100, 100); // yellow-orange
+  let endColor = color(endHue, 100, 100); // yellow-green
+
+  textSize(30);
+  fill("black");
+  text(`Analogous (Hue:${startHue} Interval:${interval})`, 40, 250);
+
+  textSize(30);
+  fill("black");
+  text("Interval", 600, 250);
+
+  drawPalette(startColor, endColor, 40, 520, 260, 12);
+  let analogousInterval = intervals * 2;
+  let analogousEndHue = startHue + analogousInterval * 12;
+  let analogousStartColor = color(startHue, 100, 100); // yellow-orange
+  let analogousEndColor = color(analogousEndHue, 100, 100); // yellow-green
+
+  textSize(30);
+  fill("black");
+  text(`Analogous (Hue:${startHue} Interval:${analogousInterval})`, 40, 350);
+
+  textSize(30);
+  fill("black");
+  text("Hue", 600, 350);
+
+  drawPalette(analogousStartColor, analogousEndColor, 40, 520, 360, 12);
+
+  textSize(30);
+  fill("black");
+  text("Rectangle Palette with Spaces", 40, 450);
+
+  textSize(30);
+  fill("black");
+  text("Spacing", 600, 450);
+  rectPalette(analogousStartColor, analogousEndColor, 40, 520, 460, 12);
 }
 
 function lerpBrightness(st_brightness, end_brightness, amt) {
@@ -16,10 +73,10 @@ function lerpBrightness(st_brightness, end_brightness, amt) {
 }
 
 function monoPalette() {
-  let st_x = 50;
+  let st_x = 40;
   let end_x = 520;
-  let st_y = height / 8;
-  let my_hue = hue("yellow");
+  let st_y = 160;
+  let my_hue = startHue;
   let st_brightness = 0;
   let end_brightness = 100;
   let divisions = 12;
@@ -28,9 +85,6 @@ function monoPalette() {
   let side = (end_x - st_x) / divisions;
   console.log(side);
 
-  textSize(30);
-  fill("black");
-  text("Monochromatic", 50, 100);
   for (let i = 0; i < divisions; i++) {
     let brightness = lerpBrightness(
       st_brightness,
@@ -46,15 +100,24 @@ function monoPalette() {
 
 function drawPalette(startColor, endColor, st_x, end_x, st_y, divisions) {
   let side = (end_x - st_x) / divisions;
-
-  textSize(30);
-  fill("black");
-  text("Analogous", 50, 220);
-
   for (let i = 0; i < divisions; i++) {
     let amt = i / (divisions - 1);
     let c = lerpColor(startColor, endColor, amt);
     fill(c);
     rect(st_x + i * side, st_y, side, side);
+  }
+}
+
+function rectPalette(startColor, endColor, st_x, end_x, st_y, divisions) {
+  let totalWidth = end_x - st_x;
+  let side = (totalWidth - gap * (divisions - 1)) / divisions;
+
+  for (let i = 0; i < divisions; i++) {
+    let amt = i / (divisions - 1);
+    let c = lerpColor(startColor, endColor, amt);
+    fill(c);
+
+    let x = st_x + i * (side + gap);
+    rect(x, st_y, side, side);
   }
 }
